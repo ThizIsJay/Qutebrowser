50835199f049765f0d50d7c93f68e22f41ca8e80
Jay <95102557+ThizIsJay@users.noreply.github.com>
2021-12-22 21:40:15 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,146 @@
+# Qutebrowser Configuratie
+
+# Standaard config van config-generate-py
+config.load_autoconfig(False)
+config.set('content.cookies.accept', 'all', 'chrome-devtools://*')
+config.set('content.cookies.accept', 'no-unknown-3rdparty', 'devtools://*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_version} (KHTML, like Gecko) {upstream_browser_key}/{upstream_browser_version} Safari/{webkit_version}', 'https://web.whatsapp.com/')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0', 'https://accounts.google.com/*')
+config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
+config.set('content.images', True, 'chrome-devtools://*')
+config.set('content.images', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome-devtools://*')
+config.set('content.javascript.enabled', True, 'devtools://*')
+config.set('content.javascript.enabled', True, 'chrome://*/*')
+config.set('content.javascript.enabled', True, 'qute://*/*')
+
+c.aliases = {
+        'key': "open -t https://app.keeweb.info",
+        'q': "quit",
+        'upd': "config-source",
+        'r': "reload",
+        'b': "back",
+        'f': "forward",
+}
+
+# Startpagina & Zoekmachines
+c.url.start_pages = "https://google.com"
+
+c.url.searchengines = {
+    'DEFAULT':  'https://google.com/search?hl=en&q={}',
+    ',d':       'https://duckduckgo.com/?ia=web&q={}',
+    ',g':      'https://github.com/search?o=desc&q={}&s=stars',
+    ',gi':      'https://www.google.com/search?tbm=isch&q={}&tbs=imgo:1',
+    ',aw':       'https://wiki.archlinux.org/?search={}',
+    ',ig':      'https://www.instagram.com/explore/tags/{}',
+    ',m':       'https://www.google.com/maps/search/{}',
+    ',r':       'https://www.reddit.com/search?q={}',
+    ',sr':      'https://www.reddit.com/r/{}',
+    ',tw':      'https://twitter.com/search?q={}',
+    ',w':       'https://en.wikipedia.org/wiki/{}',
+    ',yt':      'https://www.youtube.com/results?search_query={}'
+}
+# Nord colorscheme
+nord = {
+    # Polar Night
+    'nord0': '#2e3440',
+    'nord1': '#3b4252',
+    'nord2': '#434c5e',
+    'nord3': '#4c566a',
+    # Snow Storm
+    'nord4': '#d8dee9',
+    'nord5': '#e5e9f0',
+    'nord6': '#eceff4',
+    # Frost
+    'nord7': '#8fbcbb',
+    'nord8': '#88c0d0',
+    'nord9': '#81a1c1',
+    'nord10': '#5e81ac',
+    # Aurora
+    'nord11': '#bf616a',
+    'nord12': '#d08770',
+    'nord13': '#ebcb8b',
+    'nord14': '#a3be8c',
+    'nord15': '#b48ead',
+}
+
+c.colors.completion.category.bg = nord['nord0']
+c.colors.completion.category.border.bottom = nord['nord0']
+c.colors.completion.category.border.top = nord['nord0']
+c.colors.completion.category.fg = nord['nord5']
+c.colors.completion.even.bg = nord['nord1']
+c.colors.completion.odd.bg = nord['nord1']
+c.colors.completion.fg = nord['nord4']
+c.colors.completion.item.selected.bg = nord['nord3']
+c.colors.completion.item.selected.border.bottom = nord['nord3']
+c.colors.completion.item.selected.border.top = nord['nord3']
+c.colors.completion.item.selected.fg = nord['nord6']
+c.colors.completion.match.fg = nord['nord13']
+c.colors.completion.scrollbar.bg = nord['nord1']
+c.colors.completion.scrollbar.fg = nord['nord5']
+c.colors.downloads.bar.bg = nord['nord0']
+c.colors.downloads.error.bg = nord['nord11']
+c.colors.downloads.error.fg = nord['nord5']
+c.colors.downloads.stop.bg = nord['nord15']
+c.colors.downloads.system.bg = 'none'
+c.colors.hints.bg = nord['nord13']
+c.colors.hints.fg = nord['nord0']
+c.colors.hints.match.fg = nord['nord10']
+c.colors.keyhint.bg = nord['nord1']
+c.colors.keyhint.fg = nord['nord5']
+c.colors.keyhint.suffix.fg = nord['nord13']
+c.colors.messages.error.bg = nord['nord11']
+c.colors.messages.error.border = nord['nord11']
+c.colors.messages.error.fg = nord['nord5']
+c.colors.messages.info.bg = nord['nord8']
+c.colors.messages.info.border = nord['nord8']
+c.colors.messages.info.fg = nord['nord5']
+c.colors.messages.warning.bg = nord['nord12']
+c.colors.messages.warning.border = nord['nord12']
+c.colors.messages.warning.fg = nord['nord5']
+c.colors.prompts.bg = nord['nord2']
+c.colors.prompts.border = '1px solid ' + nord['nord0']
+c.colors.prompts.fg = nord['nord5']
+c.colors.prompts.selected.bg = nord['nord3']
+c.colors.statusbar.caret.bg = nord['nord15']
+c.colors.statusbar.caret.fg = nord['nord5']
+c.colors.statusbar.caret.selection.bg = nord['nord15']
+c.colors.statusbar.caret.selection.fg = nord['nord5']
+c.colors.statusbar.command.bg = nord['nord2']
+c.colors.statusbar.command.fg = nord['nord5']
+c.colors.statusbar.command.private.bg = nord['nord2']
+c.colors.statusbar.command.private.fg = nord['nord5']
+c.colors.statusbar.insert.bg = nord['nord14']
+c.colors.statusbar.insert.fg = nord['nord1']
+c.colors.statusbar.normal.bg = nord['nord0']
+c.colors.statusbar.normal.fg = nord['nord5']
+c.colors.statusbar.passthrough.bg = nord['nord10']
+c.colors.statusbar.passthrough.fg = nord['nord5']
+c.colors.statusbar.private.bg = nord['nord3']
+c.colors.statusbar.private.fg = nord['nord5']
+c.colors.statusbar.progress.bg = nord['nord5']
+c.colors.statusbar.url.error.fg = nord['nord11']
+c.colors.statusbar.url.fg = nord['nord5']
+c.colors.statusbar.url.hover.fg = nord['nord8']
+c.colors.statusbar.url.success.http.fg = nord['nord5']
+c.colors.statusbar.url.success.https.fg = nord['nord14']
+c.colors.statusbar.url.warn.fg = nord['nord12']
+c.colors.tabs.bar.bg = nord['nord3']
+c.colors.tabs.even.bg = nord['nord3']
+c.colors.tabs.even.fg = nord['nord5']
+c.colors.tabs.indicator.error = nord['nord11']
+c.colors.tabs.indicator.system = 'none'
+c.colors.tabs.odd.bg = nord['nord3']
+c.colors.tabs.odd.fg = nord['nord5']
+c.colors.tabs.selected.even.bg = nord['nord0']
+c.colors.tabs.selected.even.fg = nord['nord5']
+c.colors.tabs.selected.odd.bg = nord['nord0']
+c.colors.tabs.selected.odd.fg = nord['nord5']
+
+# Geavanceerde configuratie
+c.statusbar.show = "never" 
+
+c.content.blocking.adblock.lists = [
+        "https://raw.githubusercontent.com/Ewpratten/youtube_ad_blocklist/master/blocklist.txt",
+        "https://raw.githubusercontent.com/ThizIsJay/misc/stable/hosts",
+]
